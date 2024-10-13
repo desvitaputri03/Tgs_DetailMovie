82b4e210913c79dd92366885fceb8c5acc2b2e42
Desvita Putri <161610929+desvitaputri03@users.noreply.github.com>
2024-10-13 21:06:20 +0700

    Tugas Detail Movie

---
+++ app/src/main/java/com/mobile/tugas_detailmovie/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.mobile.tugas_detailmovie
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnRecycleMovie: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btnRecycleMovie = findViewById(R.id.btnRecycleMovie)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnRecycleMovie.setOnClickListener() {
+            val intentMenu = Intent(this@MainActivity, RecycleMovieActivity::class.java)
+            startActivity(intentMenu)
+        }
+    }
+
+}
